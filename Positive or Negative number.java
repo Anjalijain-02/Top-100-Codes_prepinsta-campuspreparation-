45094443f9b248238bd4e6f5c978ab3883f23d96
Anjali Jain <80818092+Anjalijain-02@users.noreply.github.com>
2023-08-01 20:29:34 +0530

    Create_Positive_or_Negative_Number.java

---
+++ Positive or Negative number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int x=sc.nextInt();
+	    if(x>0)
+	    {
+	        System.out.println("positive");
+	    }
+	    else if(x==0){
+	        System.out.println("equal");
+	        
+	    }
+	    else {
+	        System.out.println("negative");
+	        
+	    }
+		
+	}
+}
